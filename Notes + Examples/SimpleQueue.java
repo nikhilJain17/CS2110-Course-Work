2bc901e3076c306cf16e6da261cb62556fc4e003
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-07-08 22:27:56 -0400

    Finished queue with arrays

--- Notes + Examples/SimpleQueue.java
+++ Notes + Examples/SimpleQueue.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 /*
 * A simple queue.
@@ -18,6 +19,9 @@ class SimpleQueue {
   private final int ADD_PERSON = 0;
   private final int REMOVE_PERSON = 1;
   private final int FIND_PERSON = 2;
+  private static int front = 0;
+
+  private static ArrayList<String> queueArray = new ArrayList<String>();
 
 // The only purpose of this function is so that I don't
 // have to type "System.out.println()"
@@ -25,6 +29,7 @@ class SimpleQueue {
     System.out.println(displayThis);
   }
 
+// Gets user's choice and returns it
   public static int menu() throws IOException {
 
     int choice;
@@ -49,9 +54,59 @@ class SimpleQueue {
 
   }
 
+ // Adds person
+ public static void addPerson(String person) {
+       // add to back of queue
+       queueArray.add(person);
+ }
+
+ public static void removePerson() {
+       // Remove from front
+       queueArray.remove(front);
+       front++;
+ }
+
+ public static void displayQueue() {
+
+       for(int i = 0; i < queueArray.size(); i++) {
+             p("|" + queueArray.get(i) + "|");
+       }
+
+ }
+
   public static void main(String[] args) throws Exception {
-    System.out.println("Welcome to the queue program.");
-    menu();
+
+        InputStreamReader pipe = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(pipe);
+
+        while (true) {
+            System.out.println("Welcome to the queue program.");
+            int choice = menu(); // display the menu and get their response back
+
+            switch (choice) {
+
+                  case 1:
+                        displayQueue();
+                        break;
+
+                  case 2:
+                        p("Who do you want to add?");
+                        addPerson(reader.readLine());
+                        break;
+
+                  case 3:
+                        removePerson();
+                        break;
+
+                  case 4:
+                        displayQueue();
+                        break;
+
+
+            }
+
+      }
+
 
 
   }
