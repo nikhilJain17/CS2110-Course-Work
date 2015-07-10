7ec475f4b64877bdaffb518103731f1cdd8edb03
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-07-09 21:43:35 -0400

    Added an Iterator to cycle through the queue more easily

---
+++ HW3/Question 2/Nikhil_SuperQueue.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+
+class Nikhil_SuperQueue {
+
+      // Functions to implement:
+      // 1. Read from file (separated by line breaks OR spaces)
+      // 2. Leave queue (from ANY position)
+      // 3. Join queue (only at back)
+      // 4. Find customer by name (Handle multiple names - exception maybe?)
+      // 5. Sort customers by name (Use ASCII!)
+
+      
+
+
+
+}
---
+++ HW3/Question 2/Node.java
@@ -0,0 +1,33 @@
+// Each person will be part of a Node
+
+class Node {
+      
+      private Node next;
+      private Node previous; // Implement or not?
+      private Customer customer;
+
+      public Node (Customer setCustomer) {
+
+            customer = setCustomer;
+            next = null;
+            previous = null;
+      }
+
+      public void setNext(Node n) {
+            next = n;
+      }
+
+      public Node getNext() {
+            return next;
+      }
+
+      public void setPrevious(Node n) {
+            previous = n;
+      }
+
+      public Node getPrevious() {
+            return previous;
+      }
+
+
+}
