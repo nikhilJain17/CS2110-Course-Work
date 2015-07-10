397103f91c153849640663f341b46303577a4f6e
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-07-09 22:14:14 -0400

    Moved main method to its own file

---
+++ HW3/Question 2/Main.java
@@ -0,0 +1,21 @@
+
+/*
+* This class contains the main method
+* Basically the launchpoint of the program
+*/
+
+
+class Main {
+
+      public static void main(String[] args) {
+            p("Welcome to Nikhil Jain Inc.'s Customer Queue Program!");
+            // Now, commence input process (through file)
+      }
+
+
+      // It is a lot easier to just type p("hello") than System.out.println("")
+      // The only purpose of this function is to save time and typing
+      public static void p(String print) {
+            System.out.println(print);
+      }
+}
