b497d6cb0e21d3616f96ab99369631e7291caf12
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-07-18 11:15:37 -0400

    Finished implementing template for GUI of queues

--- HW4/src/SupermarketGui.java
+++ HW4/src/SupermarketGui.java
@@ -15,6 +15,18 @@ public class SupermarketGui {
 	JLabel queue2Speed; // speed
 	JLabel queue2Length; // length
 	
+	JLabel queue3Label; 
+	JLabel queue3Speed;
+	JLabel queue3Length;
+	
+	JLabel queue4Label;
+	JLabel queue4Speed;
+	JLabel queue4Length;
+	
+	JLabel queue5Label;
+	JLabel queue5Speed;
+	JLabel queue5Length;
+	
 	/*
 	 * 
 	 * TODO ADD SOME FANCY PICTURES OF A GROCERY STORE!
@@ -71,6 +83,48 @@ public class SupermarketGui {
 		queue2Length = new JLabel("Length: ");
 		queue2Length.setBounds(250, 85, 80, 25);
 		
+		
+		// Labels for Queue 3
+		queue3Label = new JLabel("Queue 3");
+		queue3Label.setBackground(Color.ORANGE);
+		queue3Label.setBounds(475, 25, 80, 25);
+		queue3Label.setOpaque(true);
+		queue3Label.setBorder(padding);
+		
+		queue3Speed = new JLabel("Speed: ");
+		queue3Speed.setBounds(475, 65, 80, 25);
+		
+		queue3Length = new JLabel("Length: ");
+		queue3Length.setBounds(475, 85, 80, 25);
+		
+		
+		// Labels for Queue 4
+		queue4Label = new JLabel("Queue 4");
+		queue4Label.setBackground(Color.orange);
+		queue4Label.setBounds(700, 25, 80, 25);
+		queue4Label.setOpaque(true);
+		queue4Label.setBorder(padding);
+		
+		queue4Speed = new JLabel("Speed: ");
+		queue4Speed.setBounds(700, 65, 80, 25);
+		
+		queue4Length = new JLabel("Length: ");
+		queue4Length.setBounds(700, 85, 80, 25);
+		
+		
+		// Labels for Queue 5
+		queue5Label = new JLabel("Queue 5");
+		queue5Label.setBackground(Color.orange);
+		queue5Label.setBounds(925, 25, 80, 25);
+		queue5Label.setOpaque(true);
+		queue5Label.setBorder(padding);
+	
+		queue5Speed = new JLabel("Speed: ");
+		queue5Speed.setBounds(925, 65, 80, 25);
+		
+		queue5Length = new JLabel("Length: ");
+		queue5Length.setBounds(925, 85, 80, 25);
+		
 		// each person will be represented by an x
 		
 		/*
@@ -95,7 +149,19 @@ public class SupermarketGui {
 		panel.add(queue2Length);
 		panel.add(queue2Speed);
 		
-		panel.add(queue2Label);
+		panel.add(queue3Label);
+		panel.add(queue3Length);
+		panel.add(queue3Speed);
+		
+		panel.add(queue4Label);
+		panel.add(queue4Length);
+		panel.add(queue4Speed);
+		
+		panel.add(queue5Label);
+		panel.add(queue5Length);
+		panel.add(queue5Speed);
+		
+		
 		
 		panel.setBorder(padding);
 		
