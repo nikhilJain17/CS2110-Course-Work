24c47319461cd69705f725dfa1db560e56c4b595
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-07-21 11:03:02 -0400

    This is a commit

--- Notes + Examples/Swinging Ball/src/MyGUI.java
+++ Notes + Examples/Swinging Ball/src/MyGUI.java
@@ -7,8 +7,8 @@ import javax.swing.*;
 public class MyGUI {
 	
 	private JFrame frame = new JFrame();
-	private JPanel actionPanel = new JPanel();
-	private JPanel playArea = new JPanel();
+	private JPanel actionPanel = new JPanel(); // where the buttons are
+	private JPanel playArea = new JPanel();    // where the ball is
 	private JButton addBallButton = new JButton("Add Red Ball");
 	private JButton moveBallButton = new JButton("Move The Ball");
 	
@@ -19,6 +19,20 @@ public class MyGUI {
 	public void buildTheGui() {
 		
 		frame.setLayout(new BorderLayout());
+		
+		// Set the button listeners
+		addBallButton.addActionListener(new AddBallButtonHandler());
+		moveBallButton.addActionListener(new MoveBallButtonHandler());
+		
+		// add the components 
+		actionPanel.add(addBallButton);
+		actionPanel.add(moveBallButton);
+		
+		frame.add(BorderLayout.NORTH, actionPanel);
+		frame.add(BorderLayout.SOUTH, playArea);
+		
+		frame.setSize(500, 500);
+		frame.setVisible(true);
 		
 	}
 
