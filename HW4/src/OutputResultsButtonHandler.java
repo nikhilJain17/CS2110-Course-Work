75fdb08b8b21e492d771934b4c6ffeecbad0aba4
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-07-22 15:39:28 -0400

    Removed a popup thing that glitched the program

---
+++ HW4/src/OutputResultsButtonHandler.java
@@ -0,0 +1,28 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+// button handler for outputting results
+
+public class OutputResultsButtonHandler implements ActionListener {
+
+	@Override
+	public void actionPerformed (ActionEvent e) {
+		
+	
+		
+		
+		// output to file
+		
+		
+		
+		
+		
+	} // end of actionPerformed
+	
+	
+	
+	
+	
+	
+} // end of classs
