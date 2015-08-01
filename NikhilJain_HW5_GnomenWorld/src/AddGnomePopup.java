12d214f710de6cfaf7358abe8d0812203821ff3c
nikhilJain17 <nikhil.jain0987@gmail.com>
2015-08-01 14:32:01 -0400

    All components added to GnomePopup, creates gnome properly

--- NikhilJain_HW5_GnomenWorld/src/AddGnomePopup.java
+++ NikhilJain_HW5_GnomenWorld/src/AddGnomePopup.java
@@ -1,5 +1,7 @@
 //import java.awt.Dimension;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
@@ -21,16 +23,22 @@ public class AddGnomePopup {
 	
 	JLabel enterGnomeLabel = new JLabel("Enter name");
 	
+	JLabel busyLabel = new JLabel("Click to change status");
+	
 	JToggleButton busyButton; // gets input if gnome is busy or not
+	boolean busyInput;
 	
 	JTextField enterGnomeName;
 	String nameInput = "";
+	
+	JButton addGnomeButton; // add gnome to village
 	
 	public AddGnomePopup(VillageNode village) {
 		
 		this.village = village;
 		enterGnomeName = new JTextField();
-		busyButton = new JToggleButton("Not busy");
+		busyButton = new JToggleButton("Not Busy");
+		addGnomeButton = new JButton("Add Gnome");
 		
 	} // end of constructor
 	
@@ -48,31 +56,58 @@ public class AddGnomePopup {
 				popup.setPreferredSize(new Dimension(400, 400));
 				popup.setTitle("Add a gnome to Village #" + village.getNamed());
 				
-				
 				// set up enterGnomeName
-				enterGnomeName.setBounds(155, 30, 200, 35);
+				enterGnomeName.setBounds(165, 30, 200, 35);
 				enterGnomeName.setPreferredSize(new Dimension(100, 35));
-				
-				
+			
 				// set up label
-				enterGnomeLabel.setBounds(50, 30, 150, 35);
+				enterGnomeLabel.setBounds(20, 30, 150, 35);
+				
+				// set up busyLabel
+				busyLabel.setBounds(20, 80, 150, 35);
 				
 				// setup busy button
-				busyButton.setBounds(155, 80, 200, 35);
+				busyButton.setBounds(165, 80, 200, 35);
 			    busyButton.addChangeListener(new ChangeListener() {
 			        @Override
 			        public void stateChanged(ChangeEvent event) {
 			            if (busyButton.isSelected()){
 			            	busyButton.setText("Busy");
 			            } else {
-			            	busyButton.setText("Not busy");
+			            	busyButton.setText("Not Busy");
 			            }
 			        }
 			    });
+			    
+			    // add the gnome with this button
+			    addGnomeButton.setBounds(15, 130, 350, 35);
+			    addGnomeButton.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						
+						// get the inputs from the forms
+						nameInput = enterGnomeName.getText();
+						busyInput = busyButton.isSelected();
+						
+						// add the gnome to the villageNode
+						Gnome createdGnome = new Gnome(nameInput, busyInput, village);
+						java.util.List<Gnome> gnomeList = village.getGnomeArray();
+						gnomeList.add(createdGnome);
+						
+						// update the display on the villagepopup
+						
+						
+					}// end of actionPerformed
+			    }); // end of action listener
+			    
+			    
 				// pack and ship to screen
 				popup.add(enterGnomeName);
 				popup.add(enterGnomeLabel);
 				popup.add(busyButton);
+				popup.add(busyLabel);
+				popup.add(addGnomeButton);
 				popup.setVisible(true);
 				popup.pack();
 								
